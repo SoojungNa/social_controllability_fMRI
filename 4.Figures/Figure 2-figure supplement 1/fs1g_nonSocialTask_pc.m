c97efb54be7c20c76cd3b7b78e6945e98f31695c
SoojungNa <55287560+SoojungNa@users.noreply.github.com>
2021-05-21 16:17:28 -0400

    Add files via upload

---
+++ 4.Figures/Figure 2-figure supplement 1/fs1g_nonSocialTask_pc.m
@@ -0,0 +1,48 @@
+clear;
+addpath(genpath('C:\Users\SN\Google Drive\z.functions\plots'));
+load('C:\Users\SN\Google Drive\z.functions\plots\colors.mat');
+
+outdir = cd;
+outfile = 'plot_lines07_pc_grey.pdf';
+
+
+%% Plot inputs
+load('C:\Users\SN\Google Drive\p3.UG2_computer_beh\2.model\pc.mat');
+data = [pc_ic pc_nc];
+
+x_tick_labels = {'C','U'};
+x_tick = [1 2];
+xlim = [0.5 2.5];
+
+y_title = [sprintf('Self-reported \n controllability'), ' (%)'];
+ylim = [0 100];
+
+
+% Mean line
+design.s1.color = black;
+design.s1.linewidth = 1.5;
+
+% SEM patch
+design.s2.color = black;
+design.s2.fa = 0.3;
+
+% Individual lines
+design.s3.color = grey_light;           % black
+% design.s3.color_up = '';        % blue2
+% design.s3.color_down = '';      % red2
+% design.s3.color_stay = '';      % black
+% design.s3.colormap = 'jet';     % 'jet', 'parula'
+design.s3.fa = 0.3; %0.3
+
+
+% Draw plot
+F = plot_lines07(data, ...
+    x_tick_labels, x_tick, xlim, ...
+    y_title, ylim, ...
+    design);
+
+%ytickformat('%3.0f');
+
+% Save
+cd(outdir);
+saveas(F, outfile);
